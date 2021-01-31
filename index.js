5f90e878f305b41542a50663c0be7c490e35955f
Andrea <andreavlas97@gmail.com>
2021-01-31 15:32:08 +0200

    designed recommanded book and redirect user when onclick on the book

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 const form = document.querySelector("#searchForm");
 const input = document.querySelector("#input");
 const container = document.querySelector("#container");
-const recommend = document.querySelector("#recommendation");
+const recommendationWrapper = document.querySelector("#recommendation-container");
 const empty = document.querySelector("#noBooksFound");
 empty.style.display = "none";
 
-// get recomanded book
+// get recommended book
 axios
   .get("https://www.googleapis.com/books/v1/volumes/hjEFCAAAQBAJ")
-  .then(({ data }) => {
-    getFavouriteBookTemplate(data);
+  .then(({data}) => {
+    getFavoritesBookTemplate(data);
+    recommendationWrapper.innerHTML = getFavoritesBookTemplate(data)
   });
 
-const getFavouriteBookTemplate = (data) => {
+  const trimString = (string) =>{
+    const length = 650;
+    if(string.length>length){
+        return string.substring(0, length) + "..."
+    }
+    return string
+  }
+
+const getFavoritesBookTemplate = (data) => {
   const {
     imageLinks = "",
     authors = "",
     title = "",
-    pageCount = "",
     categories = [],
     description = "",
     averageRating = 0,
   } = data.volumeInfo;
 
   return `
-    <div class = "recomandations">
-      <img id ="firstImage" img src=${firstImageLink}/>
-      <div class ="firstDetails>
-      <div class = "firstAuthor" >${firstBookAuthor}</div>
-      <div class = "firstTitle" >${firstBooktitle}</div>
-      <div class = "firstPage" >${firstBookPageCount}</div>
-      <div class = "firstCategory" >${firstBookCategory}</div>
+    <div class = "recommendation">
+      <img class ="recommendation__image" src=${imageLinks.thumbnail}/>
+      <div class ="recommendation-info">
+        <div class = "recommendation-info__title" >${title}</div>
+        <div class = "recommendation-info__author" >${authors}</div>
+        <div class = "recommendation-info__rating" >${averageRating}</div>
+
+        <div class = "recommendation-info__description" >${trimString(description)}</div>
+        <div class = "recommendation-info__category" >${categories}</div>
       </div>
     </div>
   `;
 };
-const displayFirstBook = (first) => {
-  contentHTML += getFirstBook(first);
-  body.innerHTML = contentHTML;
-};
+
 
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
@@ -54,15 +61,14 @@ form.addEventListener("submit", async function (e) {
   console.log(response);
 
   if (response.data.totalItems !== 0) {
-    recommend.style.display = "initial";
+    recommendationWrapper.style.display = "initial";
     displayFoundBooks(response.data.items);
   } else {
     empty.style.display = "initial";
     container.style.display = "none";
   }
 });
-// BEM = BLOCK ELEMENT MODIFIER
-// block__element--modifier
+
 const getHTMLBookFromTemplate = (data) => {
   const {
     imageLinks = "",
@@ -70,9 +76,12 @@ const getHTMLBookFromTemplate = (data) => {
     title = "",
     pageCount = "",
     categories = [],
+    previewLink = "",
+    canonicalVolumeLink = ""
   } = data.volumeInfo;
+
   return `
-    <div class = "book">
+    <div class="book" onclick="window.open('${previewLink || canonicalVolumeLink}', '_blank')">
       <img class="book__image" src=${imageLinks.thumbnail}/>
 
       <div class ="book-information">
